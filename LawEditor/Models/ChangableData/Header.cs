e55d68daa3f441e5019a06dbdc13a0bb4c80ae30
AlsuSharapova <alsusharapova1011@gmail.com>
2026-04-08 12:40:29 +0400

    Added Class Header

---
+++ LawEditor/Models/ChangableData/Header.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawEditor.Models.ChangableData {
+    public class Header {
+        public int Id { get; set; }
+        public string HeaderName { get; set; }
+        public string FullText { get; set; }
+    }
+}
